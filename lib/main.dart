c860de74a66980ce785b39b72a2deb3b904dd898
gospodi <gospodijazzet@gmail.com>
2020-04-26 17:00:28 +0300

    add StatefulWidget, name Ball

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
@@ -15,7 +16,21 @@ class BallPage extends StatelessWidget {
         backgroundColor: Colors.blue.shade900,
         title: Text('Спроси меня о чем-то'),
       ),
-      body: Container(),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Image.asset('images/ball1.png'),
     );
   }
 }
